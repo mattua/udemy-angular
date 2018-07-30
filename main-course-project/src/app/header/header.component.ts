fd0ba858b2cd6c43333fa8e80df8f7fc2eda1e35
Matt Thomas <Matt@Thomas.com>
2018-07-30 22:32:47 +0100

    boom

---
+++ main-course-project/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "node_modules/@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+
+
+})
+export class HeaderComponent {
+
+}
