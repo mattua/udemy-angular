206fb2fcf9d12c97e941ac2416117f579d518ec9
Matt Thomas <Matt@Thomas.com>
2018-07-31 12:46:37 +0100

    lecture 49 - awesome - got images working

--- main-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ main-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import {Recipe} from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes:Recipe[]= [
-    new Recipe('A Test Recipe','This is a test','https://pxhere.com/en/photo/1376204')
+    new Recipe('A Test Recipe','This is a test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg'),
+    new Recipe('A Test Recipe','This is a test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg')
   ];
 
 
