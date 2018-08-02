be3c04dd26d832f153b743d65aa10af8c0c11676
Matt Thomas <Matt@Thomas.com>
2018-08-02 21:49:02 +0100

    lecture 63 adding servers works now

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -8,6 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type:'server',name:'Testserver',content:'Just a test!'}];
   //serverElements = [];
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverName
+    });
+    
+  }
 
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+    
+  }
   
 }
