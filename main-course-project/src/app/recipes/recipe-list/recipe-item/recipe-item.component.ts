24a21160ba825f4e8ce805ae0cd87df56f04af71
Matt Thomas <Matt@Thomas.com>
2018-08-05 19:21:40 +0100

    79

--- main-course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ main-course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+
+  //this enables us to bind to this property from outside the component
+  @Input() recipe:Recipe;
 
   constructor() { }
 
