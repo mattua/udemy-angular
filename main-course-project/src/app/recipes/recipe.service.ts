5549d75be086925fa2bcd2a6c4afa158dd4aba41
Matt Thomas <Matt@Thomas.com>
2018-08-13 08:31:10 +0100

    111 added receipe ingredeients to receipe detail pahe

--- main-course-project/src/app/recipes/recipe.service.ts
+++ main-course-project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Recipe } from "./recipe.model";
 import { ThrowStmt } from "../../../node_modules/@angular/compiler";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 
 export class RecipeService {
@@ -10,8 +11,16 @@ export class RecipeService {
     // array of recipes
 
     private recipes:Recipe[]= [
-        new Recipe('A Test Recipe','This is a test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg'),
-        new Recipe('Another Test Recipe','This is another test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg')
+        new Recipe('Tasty Schnitzel','This is a test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg',
+        [
+            new Ingredient('Meat',1),
+            new Ingredient('French Fries',20)
+        ]),
+        new Recipe('Big Fat Burger','This is another test','https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2094/letterbox_ChickenChorizoPaella593x426.jpg',
+        [
+            new Ingredient('Buns',2),
+            new Ingredient('Meat',1)
+        ])
       ]
 
     
