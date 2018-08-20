70b0145f94614420d27a9b1c23fad86c77e6e975
Matt Thomas <Matt@Thomas.com>
2018-08-20 12:54:12 +0100

    122 back on track: adding parameter to route paths in app module

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
@@ -16,6 +17,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   {path:'',component:HomeComponent},
   {path:'users',component:UsersComponent},
+  {path:'users/:id',component:UserComponent},
   {path:'servers',component:ServersComponent}
 
 ]
